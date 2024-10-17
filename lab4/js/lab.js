ddc51576a5618d0ffe8d10a5a5260a187d9c6c14
Chad O. <cpordone@ucsc.edu>
2024-10-16 22:29:31 -0700

    Pizzadilla Recipe
    Added a pizzadilla recipe to the website itself.

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
+// Connect Four
+//An implementation of the (board) game.
+// Author: Chad Ordonez
 // Date:
 
 // Constants
 
 // Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
 // let's get this party started
 main();
+
+//Print the board before each turn
+  //Add 
+//Accept user input
+  //Ask user for input
+  //Proceed
+//Validate user move
+  //Make error message if move is invalid
+  //Proceed normally if not
+  //Update the board (Add user’s icon)
+//Evaluate possible moves
+//Choose computer move
+  //Choose valid move
+  //Update the board (Add computer’s icon)
+//Test for win or draw condition
+  //Win Condition: 4 icons are in a row horizontally, vertically, or diagonally
+  //Draw Condition: No more possible moves, and win condition isn’t reached
