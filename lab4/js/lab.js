4c1c4964861dd2dad55ff0a79b67b8f099989335
Chad O. <cpordone@ucsc.edu>
2024-10-17 22:12:00 -0700

    Added lots of Comments

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,7 +1,7 @@
 // Connect Four
 //An implementation of the (board) game.
 // Author: Chad Ordonez
-// Date:
+// Date: 10/17/2024
 
 // Constants
 
@@ -11,18 +11,26 @@
 main();
 
 //Print the board before each turn
-  //Add 
+  //Loop through each row
+    //Loop through each space
+      //Print X, O, or space
+      //Print a | in between
+    //Print a new line at the end
+    //Print a horizontal line in between
 //Accept user input
   //Ask user for input
   //Proceed
 //Validate user move
   //Make error message if move is invalid
   //Proceed normally if not
-  //Update the board (Add user’s icon)
+  //Update the board (Add user’s icon: O)
 //Evaluate possible moves
+  //Check columns if they have an empty space
+  //Mark columns with empty space valid
 //Choose computer move
   //Choose valid move
-  //Update the board (Add computer’s icon)
+  //Update the board (Add computer’s icon: X)
 //Test for win or draw condition
   //Win Condition: 4 icons are in a row horizontally, vertically, or diagonally
-  //Draw Condition: No more possible moves, and win condition isn’t reached
+  //Draw Condition: No more possible moves, and win condition isn’t reached.
+
