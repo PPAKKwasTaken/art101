7a3177d0ab27878b1ddb2fefa801f84f43e946d0
Chad O. <cpordone@ucsc.edu>
2024-10-26 22:55:10 -0700

    Added Javascript and Updated HTML

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// A list of car-related variables to display on the page
+// Author: Chad Ordonez
+// Date: 10/26/2024
 
-// Constants
+//Define Variables
+make = "Honda";
+model = "Odyssey";
+color = "Purple";
+year = "2000";
+ownIt = false;
 
-// Functions
+//Calculate the Car's Age
+age = (2024 - year);
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Output (Shamelessly taken from lab example)
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Do I own this?: " + ownIt + "<br>");
+document.writeln("Age: " + age + "<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
 main();
