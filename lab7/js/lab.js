1d62b8a8581790e38db40bc40406c7016d6cfcb9
Chad O. <cpordone@ucsc.edu>
2024-10-28 19:34:08 -0700

    Created Lab 7 File
    (+)HELLO WORLD!!

---
+++ lab7/js/lab.js
@@ -0,0 +1,18 @@
+//
+//
+//Author: Chad Ordonez
+//Date: 10/28/2024
+
+function userNameAlphabetize() {
+    var.userName = window.prompt("What is your name?");
+    //Asks user for their name.
+
+    sortName = userName.toLowerCase().split('').sort().join('');
+    //Turns a name into an array, sorts it and joins it back together
+
+    return sortName;
+}
+
+//output
+document.writeln("I've fixed your name without your permission! Here you go!",
+    userNameAlphabetize(), "</br>");
