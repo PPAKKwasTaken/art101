7793676e039c28e16294eacf59fa8ad2bbdfe5c8
Chad O. <cpordone@ucsc.edu>
2024-10-28 20:01:42 -0700

    Added New Function

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -13,6 +13,22 @@ function userNameAlphabetize() {
     return sortName;
 }
 
+function userNameCapitalize(name){
+    var userName = name;
+    //Takes the name the user originally typed
+
+    splitName = userName.split('');
+    //splits the name
+
+    properName = splitName[0].toUpperCase().join('');
+    //capitalizes the first letter in splitName, then recombines the list into a new name!
+    
+
+}
+
 //output
 document.writeln("I've fixed your name without your permission! Here you go!: ",
     userNameAlphabetize(), "</br>");
+
+    document.writeln("Oh wait. I forgot to capitalize it!: ",
+        userNameCapitalize(userNameAlphabetize), "</br>");
