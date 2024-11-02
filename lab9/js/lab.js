76ee20502e6535a2a66bf6678e36675b281f3011
Chad O. <cpordone@ucsc.edu>
2024-11-01 19:11:33 -0700

    added a comment

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -20,6 +20,7 @@ $(".button-minor-section").click(function(){
     $(this).parent().toggleClass("special");
     console.log($(this).parent());
 
+    //Changes the message (and color) of text on the button.
     if ($(this).html() == "UNPINK-IFY MY TEXT!" ){
         $(this).html("<span>PINK</span>-IFY MY TEXT!")
     }
