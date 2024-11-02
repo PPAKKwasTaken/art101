0972f13ce94c973f07ce8404476bf7cad6804968
Chad O. <cpordone@ucsc.edu>
2024-11-01 18:04:46 -0700

    Modified buttons

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -13,7 +13,7 @@ Date: 11/1/2024
 $(".minorSection").append("<button id='button-minorSection'>PINK-IFY MY TEXT</button>");
 
 // add a click listener to the challenge button
-$("button-minorSection").click(function(){
+$("#button-minorSection").click(function(){
     // now add (or subtract) the "special" class to the section
     $(".minor-section").toggleClass("special");
 });
