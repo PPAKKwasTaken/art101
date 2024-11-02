9d715a55cf8eebee61b4e4a94f482677ad783ded
Chad O. <cpordone@ucsc.edu>
2024-11-01 17:25:17 -0700

    Test button added

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,9 +8,20 @@ Author: Chad Ordonez
 Date: 11/1/2024
 */
 
+
+// add button to challenge section
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("button-challenge").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+});
+
+/*
 // add a click listener to all minor sections
 $("button-minor-section").click(function(){
     // now add (or subtract) the "special" class to the section
     $(".minor-section").toggleClass("special");
 });
-
+*/
