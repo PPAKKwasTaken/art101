2d4381e0a112ab19c756332c27d84a187389843b
Chad O. <cpordone@ucsc.edu>
2024-11-01 17:03:16 -0700

    Shamelessly ripped off code!

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,19 +1,18 @@
 /*
 lab.js
 
-Function: Uses buttons
+Function: Uses buttons to 
 Requirements: jQuery is loaded
 
 Author: Chad Ordonez
 Date: 11/1/2024
-
 */
 
+// adds a button to output section
+$("#output").append("<button id='button-challenge'>Make Special</button>");
 
-
-//output
-document.writeln("I've fixed your name without your permission! Here you go!: ",
-    userNameAlphabetize(), "</br>");
-
-    document.writeln("Oh wait. I forgot to capitalize it!: ",
-        userNameCapitalize(userNameAlphabetize), "</br>");
+// add a click listener to the output button
+$("button-output").click(function(){
+    // now add (or subtract) the "special" class to the section
+    $("#output").toggleClass("special");
+});
