e4c2a07be1c1f2058ab9750322a3dc0b85d2e550
Chad O. <cpordone@ucsc.edu>
2024-11-01 17:21:52 -0700

    Re added click listening

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -8,9 +8,6 @@ Author: Chad Ordonez
 Date: 11/1/2024
 */
 
-// adds a button to output section
-$(".minor-section").append("<button id='button-minorSection'>PINK-IFY MY TEXT!</button>");
-
 // add a click listener to all minor sections
 $("button-minor-section").click(function(){
     // now add (or subtract) the "special" class to the section
