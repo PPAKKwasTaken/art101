7c37ff2561708909aae4798be3f76adf81a85cfe
Chad O. <kerbhleck@gmail.com>
2024-11-14 23:57:34 -0800

    Idk

---
+++ lab12/js/lab.js
@@ -0,0 +1,15 @@
+/*
+lab.js
+
+Function: Sorts a text input into an alphetized anagram
+Requirements: jQuery is loaded
+Notes: The following code heavily borrows from the assignment template.
+
+Author: Chad Ordonez
+Date: 11/14/2024
+*/
+
+function sortingHat(str){
+    length = str[i];
+    
+}
---
+++ lab13/js/lab.js
@@ -0,0 +1,32 @@
+/*
+lab.js
+
+Function: Sorts a text input into an alphetized anagram
+Requirements: jQuery is loaded
+Notes: The following code heavily borrows from the assignment template.
+
+Author: Chad Ordonez
+Date: 11/14/2024
+*/
+
+/*
+for(let i=1;i<=100;i++){
+    //if multiple of 15
+    if (i%15 == 0) {
+      console.log("FizzBuzz!");
+    }
+    
+    else if (i%3 == 0){
+      console.log("Fizz")
+    }
+    
+    else if (i%5 == 0){
+      console.log("Buzz");
+    }
+    
+    else{
+    console.log(i);
+    }
+  }
+
+*/
