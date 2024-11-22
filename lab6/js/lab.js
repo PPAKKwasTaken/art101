eb08f0d7892e6b7fd571209487066cb853d891a6
Chad O. <kerbhleck@gmail.com>
2024-11-21 23:35:29 -0800

    Deleted main();

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -18,5 +18,3 @@ const myTransport = ["E-bike", "Loop Bus", "Metro Bus", "Car"]
 document.writeln("My main ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 document.writeln("My main transport: <pre>", JSON.stringify(myTransport, null, "\t"), "</pre>");
-
-main();
