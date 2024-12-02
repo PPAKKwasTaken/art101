c00202ff73343b57091dd1e8c9f8c297909346de
Chad O. <kerbhleck@gmail.com>
2024-12-02 09:42:02 -0800

    Added lab16 webpage

---
+++ lab16/js/lab.js
@@ -0,0 +1,12 @@
+/*
+lab.js
+
+Function:
+Requirements: jQuery is loaded
+Notes: The following code heavily borrows from the assignment template and a jsbin excercise we did in class.
+
+Author: Chad Ordonez
+Date: 12/1/2024
+*/
+
+
